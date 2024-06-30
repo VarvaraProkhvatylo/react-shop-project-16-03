7e7024c2d1ee4fa3aeabcc1a6e055b432b5539bc
Varvara <varvara.prokhvatylo@gmail.com>
2024-06-30 19:44:14 +0300

    create element in tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//create react element
+const h1 = React.createElement('h1', { id: 'title' }, 'Hello app')
+const p = React.createElement('p', null, 'Blablabla blabla blablabla')
+
+const app = (
+    <div>
+        {h1}
+        {p}
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
