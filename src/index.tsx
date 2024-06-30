f961b40d760b3e7156a1683c43e83891f840e248
Varvara <varvara.prokhvatylo@gmail.com>
2024-06-30 19:53:12 +0300

    created react element with jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//create react element
+//create react element without jsx
 const h1 = React.createElement('h1', { id: 'title' }, 'Hello app')
 const p = React.createElement('p', null, 'Blablabla blabla blablabla')
+
+/// create react element with jsx
+
+const ul = (
+    <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+    </ul>
+)
 
 const app = (
     <div>
         {h1}
         {p}
+        {ul}
     </div>
 )
 
