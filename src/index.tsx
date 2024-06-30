1fc52da2a1c947cb97c642d522e0d2ca5d9757a8
Varvara <varvara.prokhvatylo@gmail.com>
2024-06-30 20:16:38 +0300

    chenged and created element via Arrow Function

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Title() {
-    return <h1>Hello Title Component</h1>
-}
+const Title = () => <h1>Hello Title Component</h1>
 
 function Content() {
     return (
@@ -16,10 +14,21 @@ function Content() {
     )
 }
 
+/// Component via Arrow Function
+
+const Content1 = () => (
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus et
+        quam vel aut nemo fuga. Exercitationem sit dolores minus nisi repellat
+        beatae odit eligendi repellendus iste, dolorum soluta ducimus eos.
+    </p>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <Title />
         <Content />
+        <Content1 />
     </React.StrictMode>
 )
