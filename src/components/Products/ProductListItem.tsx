431bfbf35b1b27b439beacaa5ef26eba21e1fe00
Varvara <varvara.prokhvatylo@gmail.com>
2024-07-20 20:12:29 +0300

    added dynamic data via props in ListItems

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,19 +1,27 @@
 import { Button, Card, CardContent } from '@mui/material'
+import './ProductListItem.css'
 
-type Props = {}
+type Props = {
+    title: string
+    description: string
+}
 
-const ProductListItem = (props: Props) => {
+const ProductListItem = ({ title, description }: Props) => {
     return (
-        <Card>
-            <CardContent>
-                <h3>iPhone 15</h3>
-                <p>This is iPhone 15</p>
-                <div>Type: phone</div>
-                <div>Capacity: 64 GB</div>
-                <div>$ 800</div>
-                <Button>Add to cart</Button>
-            </CardContent>
-        </Card>
+        <>
+            <Card variant="outlined" className="product-list-item">
+                <CardContent>
+                    <h3 className="product-title">{title}</h3>
+                    <p className="product-description">{description}</p>
+                    <div className="product-features">Type: phone</div>
+                    <div className="product-features">Capacity: 64 GB</div>
+                    <div className="product-price">$ 800</div>
+                    <div className="btns-wrapper">
+                        <Button variant="outlined">Add to cart</Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </>
     )
 }
 export default ProductListItem
