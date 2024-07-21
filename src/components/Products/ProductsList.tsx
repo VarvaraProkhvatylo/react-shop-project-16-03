c228b980655bd666c7183a4939aa95f41cdfa3c6
Varvara <varvara.prokhvatylo@gmail.com>
2024-07-21 19:55:48 +0300

    product data destructured in prductsArray.map, added id

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -17,17 +17,19 @@ const ProductsList = (props: Props) => {
                 Product List
             </Typography>
             <Grid container spacing={2}>
-                {productsArray.map((product, i) => (
-                    <Grid item xs={12} sm={6} lg={4} key={i}>
-                        <ProductListItem
-                            title={product.title}
-                            description={product.description}
-                            type={product.type}
-                            capacity={product.capacity}
-                            price={product.price}
-                        />
-                    </Grid>
-                ))}
+                {productsArray.map(
+                    ({ id, title, description, type, capacity, price }) => (
+                        <Grid item xs={12} sm={6} lg={4} key={id}>
+                            <ProductListItem
+                                title={title}
+                                description={description}
+                                type={type}
+                                capacity={capacity}
+                                price={price}
+                            />
+                        </Grid>
+                    )
+                )}
             </Grid>
         </>
     )
--- src/utils/productsArray.ts
+++ src/utils/productsArray.ts
@@ -1,4 +1,5 @@
 type Product = {
+    id: number
     title: string
     description: string
     type: string
@@ -8,6 +9,7 @@ type Product = {
 
 export const productsArray:Product[] = [
     {
+        id: 1,
         title: "iPhone 15",
         description: "This is iPhone 15",
         type: "phone",
@@ -15,6 +17,7 @@ export const productsArray:Product[] = [
         price: 1000,
     },
     {
+        id: 2,
         title: "iPhone 14",
         description: "This is iPhone 14",
         type: "phone",
@@ -22,6 +25,7 @@ export const productsArray:Product[] = [
         price: 850,
     },
     {
+        id: 3,
         title: "iPhone 13",
         description: "This is iPhone 13",
         type: "phone",
@@ -29,6 +33,7 @@ export const productsArray:Product[] = [
         price: 700,
     },
     {
+        id: 4,
         title: "iPhone 14 Pro",
         description: "This is iPhone 14 Pro",
         type: "phone",
@@ -36,6 +41,7 @@ export const productsArray:Product[] = [
         price: 600,
     },
     {
+        id: 5,
         title: "iPhone 12 Pro",
         description: "This is iPhone 12 Pro",
         type: "phone",
@@ -43,6 +49,7 @@ export const productsArray:Product[] = [
         price: 1000,
     },
     {
+        id: 6,
         title: "iPhone 11",
         description: "This is iPhone 11",
         type: "phone",
