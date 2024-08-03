84c8f30f7ab3e0c7c891e376506ca1a57b2743e2
Varvara <varvara.prokhvatylo@gmail.com>
2024-08-03 20:23:22 +0300

    added counting of products' number and price for Cart

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -17,10 +17,20 @@ const App = () => {
         totalPrice: 0,
     })
 
+    const addProductToCart = (count: number, price: number) => {
+        setCartData((prevState) => ({
+            totalCount: prevState.totalCount + count,
+            totalPrice: prevState.totalPrice + count * price,
+        }))
+    }
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header cartData={cartData} />
+            <button onClick={() => addProductToCart(5, 1000)}>
+                Add to cart (5, 1000$)
+            </button>
             <Main />
             <Footer />
         </StyledEngineProvider>
